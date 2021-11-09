3ab8c17d5f1630dbabbd46c6a4189fe4e6f6c115
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-09 17:25:19 +0900

    [mod]一覧ページいったん表示

--- wordpress/wp-content/themes/store-coffee/archive-news.php
+++ wordpress/wp-content/themes/store-coffee/archive-news.php
@@ -1,5 +1,28 @@
 <?php get_header(); ?>
 
-<?php the_content(); ?>
+<section id="news" class="l-main-news">
+	<div class="l-main-section-visual news">
+		<h2 class="l-main-title">NEWS</h2>
+	</div>
+	<div class="content">
+		<div class="news-list">
+			<?php
+				$args = array(
+				'post_type' => 'news',
+				'posts_per_page' => 10,
+				);
+				$news_query = new WP_Query($args);
+				if($news_query->have_posts()):
+				while($news_query->have_posts()):
+				$news_query->the_post();
+			?>
+			<dl>
+				<dt><?= get_the_date(); ?></dt>
+				<dd><a href="<?php the_permalink(); ?>"><?= get_the_title(); ?></a></dd>
+			</dl>
+			<?php endwhile; endif; ?>
+        </div>
+	</div>
+</section><!-- /.l-main-news -->
 
 <?php get_footer(); ?>
