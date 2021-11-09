1e5b4bb821b85e166aa4bfc7b0af1d8b21f874e8
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-09 18:30:00 +0900

    [mod]single-newsスタイル調整

--- wordpress/wp-content/themes/store-coffee/single-news.php
+++ wordpress/wp-content/themes/store-coffee/single-news.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
 
-<section id="news" class="l-main-news">
+<section id="news" class="l-main-news single-news">
 	<div class="l-main-section-visual news">
 		<h1 class="l-main-title">NEWS</h2>
 	</div>
 	<div class="content">
         <h2><?= get_the_title(); ?></h2>
         <p><?= get_the_date(); ?></p>
-        <div><?php the_content(); ?></div>
+        <div class="entry-field"><?php the_content(); ?></div>
+        <a href="<?php echo home_url('/news/'); ?>" class="btn-more">BACK</a>
 	</div>
-    <a href="<?php echo home_url('/news/'); ?>" class="btn-more">BACK</a>
 </section><!-- /.l-main-news -->
 
 <?php get_footer(); ?>
