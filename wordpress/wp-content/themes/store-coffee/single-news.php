6e8db63f3e5441f6bf7aa01eae6c6a79d8e0bbf1
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-09 18:40:47 +0900

    [mod]ニュース詳細スタイル調整

--- wordpress/wp-content/themes/store-coffee/single-news.php
+++ wordpress/wp-content/themes/store-coffee/single-news.php
@@ -5,8 +5,8 @@
 		<h1 class="l-main-title">NEWS</h2>
 	</div>
 	<div class="content">
-        <h2><?= get_the_title(); ?></h2>
-        <p><?= get_the_date(); ?></p>
+        <h2 class="title"><?= get_the_title(); ?></h2>
+        <p class="date"><?= get_the_date(); ?></p>
         <div class="entry-field"><?php the_content(); ?></div>
         <a href="<?php echo home_url('/news/'); ?>" class="btn-more">BACK</a>
 	</div>
