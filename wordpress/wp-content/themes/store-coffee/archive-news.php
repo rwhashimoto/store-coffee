8e99f3dc2240e43aaf0daf5aca8084900988eac9
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-10 18:10:45 +0900

    [mod]全権表示へ修正

--- wordpress/wp-content/themes/store-coffee/archive-news.php
+++ wordpress/wp-content/themes/store-coffee/archive-news.php
@@ -9,7 +9,7 @@
 			<?php
 				$args = array(
 				'post_type' => 'news',
-				'posts_per_page' => 10,
+				'posts_per_page' => -1,
 				);
 				$news_query = new WP_Query($args);
 				if($news_query->have_posts()):
