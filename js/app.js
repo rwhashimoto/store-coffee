34091f40aa7fcce7258a3bf3183e905f68248f25
koya <koya.a.85@gmail.com>
2021-11-10 18:28:20 +0900

    [ADD]初期ファイル再追加

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(function(){//smooth scroll
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
+
+$(function () {//page top
+  var pagetop = $('#top-btn');
+  pagetop.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
