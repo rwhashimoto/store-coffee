ab020f36edd58be6f32231ae97ac1758945d2be9
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-10 15:39:20 +0900

    [del]aboutページからlocationコンテンツ削除

--- wordpress/wp-content/themes/store-coffee/page-about.php
+++ wordpress/wp-content/themes/store-coffee/page-about.php
@@ -47,10 +47,4 @@
 	</div><!-- /.content-wrapper -->
 </section><!-- /.l-man-about -->
 
-<?php
-$page_id = 17;
-$alternative_post = get_post( $page_id );
-echo apply_filters('the_content', $alternative_post->post_content);
-?><!-- .l-main-location -->
-
 <?php get_footer(); ?>
