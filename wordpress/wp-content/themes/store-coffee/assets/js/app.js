b90514fee153bf1c9dceb55acf1de0648188146e
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-10 18:20:17 +0900

    [mod]jquery記述方法修正

--- wordpress/wp-content/themes/store-coffee/assets/js/app.js
+++ wordpress/wp-content/themes/store-coffee/assets/js/app.js
@@ -1,28 +1,30 @@
-jQuery(function(){//smooth scroll
-    jQuery('a[href^="#"]').click(function(){
+jQuery(function ($) {
+  $(function () {//smooth scroll
+    $('a[href^="#"]').click(function () {
       var speed = 500;
-      var href= jQuery(this).attr("href");
-      var target = jQuery(href == "#" || href == "" ? 'html' : href);
+      var href = $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
       var position = target.offset().top;
-      jQuery("html, body").animate({scrollTop:position}, speed, "swing");
+      $("html, body").animate({ scrollTop: position }, speed, "swing");
       return false;
     });
   });
 
-jQuery(function () {//page top
-  var pagetop = jQuery('#top-btn');
-  pagetop.hide();
-  jQuery(window).scroll(function () {
-    if (jQuery(this).scrollTop() > 500) {
-      pagetop.fadeIn();
-    } else {
-      pagetop.fadeOut();
-    }
-  });
-  pagetop.click(function () {
-    jQuery('body, html').animate({
+  $(function () {//page top
+    var pagetop = $('#top-btn');
+    pagetop.hide();
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 500) {
+        pagetop.fadeIn();
+      } else {
+        pagetop.fadeOut();
+      }
+    });
+    pagetop.click(function () {
+      $('body, html').animate({
         scrollTop: 0
-    }, 500);
-    return false;
+      }, 500);
+      return false;
+    });
   });
 });
