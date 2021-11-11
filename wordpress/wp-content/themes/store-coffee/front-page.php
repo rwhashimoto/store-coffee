d06ced2e37377bf85ea82935890ca274265dff2a
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-11 12:36:23 +0900

    [mod]googlemapから画像へ変更

--- wordpress/wp-content/themes/store-coffee/front-page.php
+++ wordpress/wp-content/themes/store-coffee/front-page.php
@@ -84,7 +84,9 @@
 		<div class="content">
 			<h3 class="l-main-heading">OUR STORE</h3>
 			<div class="content-flex">
-				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3242.3141167384097!2d139.70497506554915!3d35.6446317802028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b40ba660b1b%3A0x8858088b6942f55f!2z44CSMTUwLTAwMjIg5p2x5Lqs6YO95riL6LC35Yy65oG15q-U5a-_5Y2X!5e0!3m2!1sja!2sjp!4v1631173724943!5m2!1sja!2sjp" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+				<figure class="info-img">
+					<img src="<?= get_template_directory_uri(); ?>/assets/images/location_001.jpg" alt="location_001">
+				</figure>
 				<div class="info">
 					<address>
 						<p>
