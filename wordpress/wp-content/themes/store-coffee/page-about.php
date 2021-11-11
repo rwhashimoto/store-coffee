c205fe373a193066f06d428cc7aa804b91bd5b5e
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-11 14:49:45 +0900

    [mod]固定ページの出力をスラッグで指定

--- wordpress/wp-content/themes/store-coffee/page-about.php
+++ wordpress/wp-content/themes/store-coffee/page-about.php
@@ -1,18 +1,16 @@
 <?php get_header(); ?>
 
-<!-- <?php the_content(); ?> -->
-<!-- .l-main-about -->
-
 <section id="about" class="l-main-about page-about">
 	<div class="l-main-section-visual about">
 		<h2 class="l-main-title">ABOUT</h2>
 	</div>
-	<?php
+
+	<div class="content-wrapper">
+		<?php
 		$page_data = get_page_by_path('/about/coffee/');
 		$page = get_post($page_data);
 		$content = $page->post_content;
 		?>
-	<div class="content-wrapper">
 		<h3 class="l-main-heading"><?= get_the_title($page); ?></h3>
 		<div class="content"><?= $content; ?></div>
 	</div><!-- /.content-wrapper -->
