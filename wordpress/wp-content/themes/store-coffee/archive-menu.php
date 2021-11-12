55007dea89be957162bf6f269a166e010bf288d2
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-12 09:12:39 +0900

    [mod]menuページ必要情報出力

--- wordpress/wp-content/themes/store-coffee/archive-menu.php
+++ wordpress/wp-content/themes/store-coffee/archive-menu.php
@@ -6,80 +6,89 @@
 	</div>
 
 	<div class="content">
-		<div class="menu-coffee">
-			<h2 class="l-main-heading">COFFEE</h2>
-			<ul class="menu-items">
-				<?php
-				$args = array(
-					'post_type' => 'menu',
-					'posts_per_page' => -1,
-				);
-				$menu_query = new WP_Query($args);
-				$cat = get_field('menu-category');
-				if ($menu_query->have_posts()) :
-					while ($menu_query->have_posts()) :
-						$menu_query->the_post();
-						$thumbnail = get_field('thumbnail');
-						$menu_price = get_field('price');
-				?>
-						<li class="menu-item">
-							<figure class="thumbnail"><img src="<?= $thumbnail ?>" alt=""></figure>
+		<h2 class="l-main-heading">COFFEE</h2>
+		<ul class="menu-items">
+			<?php
+			$args = array(
+				'post_type' => 'menu',
+				'posts_per_page' => -1,
+			);
+			$menu_query = new WP_Query($args);
+			if ($menu_query->have_posts()) :
+				while ($menu_query->have_posts()) :
+					$menu_query->the_post();
+					$thumbnail = get_field('thumbnail');
+					$menu_price = get_field('price');
+					$cat = get_field('menu-category');
+			?>
+					<li class="menu-item">
+						<?php if ($cat == 'coffee') : ?>
+							<figure figure class="thumbnail"><img src="<?= $thumbnail ?>" alt=""></figure>
 							<p class="title"><?= get_the_title(); ?></p>
 							<p class="price">&yen;<?= $menu_price ?></p>
-						</li>
-				<?php endwhile; endif; wp_reset_postdata();?>
-			</ul>
-		</div>
+							<p class="price"><?= $cat ?></p>
+						<?php endif; ?>
+					</li>
+			<?php endwhile;
+			endif;
+			wp_reset_postdata(); ?>
+		</ul>
 
-<!-- 
-		<div class="menu-coffee">
-			<h2 class="l-main-heading">FOOD</h2>
-			<ul class="menu-items">
-				<?php
-				$args = array(
-					'post_type' => 'menu',
-					'posts_per_page' => -1,
-				);
-				$menu_query = new WP_Query($args);
-				if ($menu_query->have_posts()) :
-					while ($menu_query->have_posts()) :
-						$menu_query->the_post();
-						$thumbnail = get_field('thumbnail');
-						$menu_price = get_field('price');
-				?>
-						<li class="menu-item">
-							<figure class="thumbnail"><img src="<?= $thumbnail ?>" alt=""></figure>
+		<h2 class="l-main-heading">food</h2>
+		<ul class="menu-items">
+			<?php
+			$args = array(
+				'post_type' => 'menu',
+				'posts_per_page' => -1,
+			);
+			$menu_query = new WP_Query($args);
+			if ($menu_query->have_posts()) :
+				while ($menu_query->have_posts()) :
+					$menu_query->the_post();
+					$thumbnail = get_field('thumbnail');
+					$menu_price = get_field('price');
+					$cat = get_field('menu-category');
+			?>
+					<li class="menu-item">
+						<?php if ($cat == 'food') : ?>
+							<figure figure class="thumbnail"><img src="<?= $thumbnail ?>" alt=""></figure>
 							<p class="title"><?= get_the_title(); ?></p>
 							<p class="price">&yen;<?= $menu_price ?></p>
-						</li>
-				<?php endwhile; endif; wp_reset_postdata();?>
-			</ul>
-		</div>
+							<p class="price"><?= $cat ?></p>
+						<?php endif; ?>
+					</li>
+			<?php endwhile;
+			endif;
+			wp_reset_postdata(); ?>
+		</ul>
 
-		<div class="menu-coffee">
-			<h2 class="l-main-heading">OTHER</h2>
-			<ul class="menu-items">
-				<?php
-				$args = array(
-					'post_type' => 'menu',
-					'posts_per_page' => -1,
-				);
-				$menu_query = new WP_Query($args);
-				if ($menu_query->have_posts()) :
-					while ($menu_query->have_posts()) :
-						$menu_query->the_post();
-						$thumbnail = get_field('thumbnail');
-						$menu_price = get_field('price');
-				?>
-						<li class="menu-item">
-							<figure class="thumbnail"><img src="<?= $thumbnail ?>" alt=""></figure>
+		<h2 class="l-main-heading">other</h2>
+		<ul class="menu-items">
+			<?php
+			$args = array(
+				'post_type' => 'menu',
+				'posts_per_page' => -1,
+			);
+			$menu_query = new WP_Query($args);
+			if ($menu_query->have_posts()) :
+				while ($menu_query->have_posts()) :
+					$menu_query->the_post();
+					$thumbnail = get_field('thumbnail');
+					$menu_price = get_field('price');
+					$cat = get_field('menu-category');
+			?>
+					<li class="menu-item">
+						<?php if ($cat == 'other') : ?>
+							<figure figure class="thumbnail"><img src="<?= $thumbnail ?>" alt=""></figure>
 							<p class="title"><?= get_the_title(); ?></p>
 							<p class="price">&yen;<?= $menu_price ?></p>
-						</li>
-				<?php endwhile; endif; wp_reset_postdata();?>
-			</ul>
-		</div> -->
-	</div>
+							<p class="price"><?= $cat ?></p>
+						<?php endif; ?>
+					</li>
+			<?php endwhile;
+			endif;
+			wp_reset_postdata(); ?>
+		</ul>
 </section>
 
 <?php get_footer(); ?>
