b751fc72d12c53545a5cab5abbf7563c7519ecb6
hashimoto <m.hashimoto@risewill.co.jp>
2021-11-12 09:32:04 +0900

    [mod]フロントmenu出力件数変更

--- wordpress/wp-content/themes/store-coffee/front-page.php
+++ wordpress/wp-content/themes/store-coffee/front-page.php
@@ -38,7 +38,7 @@
 					<?php
 					$args = array(
 						'post_type' => 'menu',
-						'posts_per_page' => 10,
+						'posts_per_page' => 3,
 					);
 					$menu_query = new WP_Query($args);
 					if ($menu_query->have_posts()) :
@@ -60,7 +60,7 @@
 					<?php
 					$args = array(
 						'post_type' => 'menu',
-						'posts_per_page' => 10,
+						'posts_per_page' => 3,
 					);
 					$menu_query = new WP_Query($args);
 					if ($menu_query->have_posts()) :
