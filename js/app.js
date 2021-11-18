651a0fef78ad5b0508a7c5c71ede710e7c0fa455
koya85 <42083761+koya58@users.noreply.github.com>
2021-11-18 14:38:22 +0900

    Revert "Feaure/wordpress news (#9)" (#13)
    This reverts commit 685a4761b6e6470ea2240d0d0e9e12a90434e378.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
-jQuery(function(){//smooth scroll
-    jQuery('a[href^="#"]').click(function(){
+$(function(){//smooth scroll
+    $('a[href^="#"]').click(function(){
       var speed = 500;
-      var href= jQuery(this).attr("href");
-      var target = jQuery(href == "#" || href == "" ? 'html' : href);
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
       var position = target.offset().top;
-      jQuery("html, body").animate({scrollTop:position}, speed, "swing");
+      $("html, body").animate({scrollTop:position}, speed, "swing");
       return false;
     });
   });
 
-jQuery(function () {//page top
-  var pagetop = jQuery('#top-btn');
+$(function () {//page top
+  var pagetop = $('#top-btn');
   pagetop.hide();
-  jQuery(window).scroll(function () {
-    if (jQuery(this).scrollTop() > 500) {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
       pagetop.fadeIn();
     } else {
       pagetop.fadeOut();
     }
   });
   pagetop.click(function () {
-    jQuery('body, html').animate({
+    $('body, html').animate({
         scrollTop: 0
     }, 500);
     return false;
--- wordpress/wp-content/themes/store-coffee/footer.php
+++
@@ -1,9 +0,0 @@
-        <footer class="l-footer">
-			<div id="top-btn" class="top-btn">TOP</div>
-			<p class="copyright"><small>&copy;COFFEE</small></p>
-		</footer>
-
-		<script src="<?= get_template_directory_uri(); ?>/assets/js/app.js"></script>
-        <?php wp_footer(); ?>
-	</body>
-</html>
